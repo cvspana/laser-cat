34cf022be587dd1fe66dd9774faaa9cac396d7db
cvspana <carl.spana8@gmail.com>
2018-02-28 13:41:30 -0500

    fixed lag - make sure to convert ur backgrounds folks!

--- main2.py
+++ main2.py
@@ -11,20 +11,25 @@
  
 import pygame
 import random
- 
+pygame.init()
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.init()
 # Define some colors
 BLACK    = (   0,   0,   0)
 WHITE    = ( 255, 255, 255)
 GREEN    = (   0, 255,   0)
 RED      = ( 255,   0,   0)
 BLUE = (0,0,255)
-bg = pygame.image.load("Background1.jpg")
+bg = pygame.image.load("Background1.jpg").convert_alpha()
 bg = pygame.transform.scale(bg, [800,600])
 class Cat(pygame.sprite.Sprite):
+     catRight = pygame.image.load("cat.png").convert_alpha()
+     catLeft = pygame.image.load("cat2.png").convert_alpha()
      def __init__(self):
-         
+
          pygame.sprite.Sprite.__init__(self)
-         self.image = pygame.image.load("cat.png")
+         self.image = self.catRight
          self.rect = self.image.get_rect()
          self.rect.x = 620
          self.rect.y = 430
@@ -39,9 +44,9 @@ class Cat(pygame.sprite.Sprite):
          if pdown == True:
              self.rect.y += 7
          if press == True:
-            self.image =  pygame.image.load("cat2.png")
+            self.image =  self.catLeft
          if press == False:
-            self.image = pygame.image.load("cat.png")
+            self.image = self.catRight
          if self.rect.x >= 734:
             self.rect.x = 734
          if self.rect.x <= 0:
@@ -56,9 +61,10 @@ class Cat(pygame.sprite.Sprite):
          
              
 class Coin(pygame.sprite.Sprite):
+    coinPic = pygame.image.load("coin_Custom_Custom.png").convert_alpha()
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("coin_Custom_Custom.png")
+        self.image = self.coinPic
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(765)
         self.rect.y = random.randrange(565)
@@ -73,17 +79,18 @@ class Coin(pygame.sprite.Sprite):
         #        self.rect.y = random.randrange(465)
 class Laser(pygame.sprite.Sprite):
     velocity = 5+random.randrange(5)
+    laserPic = pygame.image.load("laser.bmp").convert_alpha()
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.image.load("laser.bmp")
+        self.image = self.laserPic
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(790)
         self.rect.y = random.randrange(100)
         self.rect.y *= -1
     def update(self):
         self.rect.y += self.velocity
-        if self.rect.y >= 550:
+        if self.rect.y >= 600:
             self.rect.y = random.randrange(100)
             self.rect.y *= -1
             self.rect.x = random.randrange(790)
@@ -91,11 +98,10 @@ class Laser(pygame.sprite.Sprite):
 
 
         
-pygame.init()
 
 
-size = (800, 600)
-screen = pygame.display.set_mode(size)
+
+
  
 pygame.display.set_caption("Laser Cat")
  
@@ -106,6 +112,8 @@ done = False
 clock = pygame.time.Clock()
 pygame.mixer.music.load('music.mp3')
 pygame.mixer.music.play(-1)
+
+
 coin = Coin()
 cat = Cat()
 laser = Laser()
@@ -138,15 +146,29 @@ all_sprites.add(cat)
 internalClock = 0
 
 
+introS = True
+
+text = "Hit Space"
+font = pygame.font.SysFont('wingdings', 40)
+def introScreen():
+    screen.fill(WHITE)
+    print("")
+
+    intro = font.render(str(text), True, BLACK)
+    screen.blit(intro,[350,300])
 
 
 # -------- Main Program Loop -----------
 while not done:
+    pygame.display.set_caption("Laser Cat" +str(clock.get_fps()))
     # --- Main event loop
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
+
         elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                introS = False
             if event.key == pygame.K_LEFT:
                 pleft = True
                 press = False
@@ -168,34 +190,41 @@ while not done:
             elif event.key == pygame.K_UP:
                 pup = False
             elif event.key == pygame.K_DOWN:
-                pdown = False            
-        
-  
-    # --- Game logic should go heredf
-    cat.update()
-    coin.update()
-    
-    lasers.update()
-    if (internalClock%1800 == 0):
-        laser = Laser()
-        laser.rect.x = random.randrange(790)
-        laser.rect.y = random.randrange(100)
-        laser.rect.y *= -1
-        all_sprites.add(laser)
-        lasers.add(laser)
-    
-    hitcoin = pygame.sprite.spritecollide(cat, coin_list, False)
-    
-    if len(hitcoin) >= 1:
-        coin.rect.x = random.randrange(765)
-        coin.rect.y =100+ random.randrange(465)
-        score += 1
+                pdown = False
 
-    hitlaser = pygame.sprite.spritecollide(cat, lasers, False)    
-    if len(hitlaser) >= 1:
+    if introS == True:
+        introScreen()
+    else:
+        #   --- Game logic should go heredf
+        cat.update()
+        coin.update()
+    
+        lasers.update()
+
+
+
+
+
+        if (internalClock%1800 == 0):
+            laser = Laser()
+            laser.rect.x = random.randrange(790)
+            laser.rect.y = random.randrange(100)
+            laser.rect.y *= -1
+            all_sprites.add(laser)
+            lasers.add(laser)
+    
+        hitcoin = pygame.sprite.spritecollide(cat, coin_list, False)
+    
+        if len(hitcoin) >= 1:
+            coin.rect.x = random.randrange(765)
+            coin.rect.y =100+ random.randrange(465)
+            score += 1
+
+        hitlaser = pygame.sprite.spritecollide(cat, lasers, False)
+        if len(hitlaser) >= 1:
         
-        done = True
-        print(score)
+            done = True
+            print(score)
 
         
     
@@ -206,21 +235,22 @@ while not done:
      
     # First, clear the screen to white. Don't put other drawing commands
     # above this, or they will be erased with this command.
-    screen.fill(WHITE)
+        screen.fill(WHITE)
     
     
-    font = pygame.font.SysFont('arial', 40)
-    lose = font.render(str("You Lose"),True,BLACK)
-    text = font.render(str(score),True,WHITE)
-    screen.blit(bg, [0,0])
-    screen.blit(text, [10,10])
-    all_sprites.draw(screen)
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-    internalClock +=1
+        # font = pygame.font.SysFont('arial', 40)
+
+        #text = font.render(str(score),True,WHITE)
+        screen.blit(bg, [0,0])
+        #screen.blit(text, [10,10])
+        all_sprites.draw(screen)
+        # --- Go ahead and update the screen with what we've drawn.
+
+        internalClock +=1
 
     # --- Limit to 60 frames per second
     clock.tick(60)
+    pygame.display.flip()
      
 # Close the window and quit.
 # If you forget this line, the program will 'hang'
