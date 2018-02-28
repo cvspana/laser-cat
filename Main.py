30f2f761b7beba32f53c978505cc23b1c55dbb00
cvspana <36903824+cvspana@users.noreply.github.com>
2018-02-27 19:55:57 -0500

    h

--- Main.py
+++ Main.py
@@ -1,1 +1,18 @@
-drfhgfgh
+import pygame
+
+pygame.init()
+
+gameDisplay = pygame.display.set_mode([800,600])
+pygame.display.set_caption('Laser Cat')
+
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
+quit()
